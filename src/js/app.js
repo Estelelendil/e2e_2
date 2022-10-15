9686c2f66c93f8f9b1aa5daca6a7379b92b2e1eb
Alexandra Chernova <estelelendil@mail.ru>
2022-10-16 00:11:38 +0300

    added logic to remove class blur

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,39 @@
-const arrCard = document.querySelectorAll('.img');
+const arrCard = Array.from(document.querySelectorAll('.img'));
 const input = document.querySelector('.input');
 const button = document.querySelector('.button');
 const mir = document.querySelector('#mir');
+const mastercard = document.querySelector('#mastercard');
+const visa = document.querySelector('#visa');
+const amix = document.querySelector('#amix');
+const diners = document.querySelector('#diners');
+const discover = document.querySelector('#discover');
+// const list = arrCard.querySelectorAll('div');
 input.addEventListener('keyup', () => {
   console.log(arrCard);
-  Array.of(arrCard).map((item) => {
-    item.map((el) => {
-      if (!el.classList.contains('blur')) {
-        console.log('blur!');
-        el.classList.add('blur');
-        return true;
-      }
-      return false;
-    });
-    console.log('kus', item[0]);
+  arrCard.forEach((item) => {
     if (!item.classList.contains('blur')) {
-      console.log('blur!');
       item.classList.add('blur');
-      return true;
     }
-    return false;
   });
 });
 
 button.addEventListener('click', () => {
   if (/^2/.test(input.value)) {
     mir.classList.remove('blur');
-    console.log('TRUE');
-  } else { console.log('FALSE'); }
+  }
+  if ((/^34/.test(input.value)) || (/^37/.test(input.value))) {
+    amix.classList.remove('blur');
+  }
+  if ((/^30/.test(input.value)) || (/^55/.test(input.value))) {
+    diners.classList.remove('blur');
+  }
+  if (/^4/.test(input.value)) {
+    visa.classList.remove('blur');
+  }
+  if ((/^52/.test(input.value)) || (/^54/.test(input.value))) {
+    mastercard.classList.remove('blur');
+  }
+  if (/^6/.test(input.value)) {
+    discover.classList.remove('blur');
+  }
 });
